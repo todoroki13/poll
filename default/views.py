05ed3b0697fd9e76148a51460ca3f4ffbe98fe61
todoroki chen <kimchen931213@gmail.com>
2021-06-17 11:56:03 +0800

    修改刪除投票主題，新增修改刪除選項

--- default/views.py
+++ default/views.py
@@ -30,8 +30,8 @@ class PollCreate(CreateView):
     model = Poll
     fields = ['subject', 'desc']
     #fields = '__all__'
+    
     #success_url = '/poll/'
-
     def get_success_url(self):
         return "/poll/{}/".format(self.object.id) 
 
@@ -45,3 +45,31 @@ class PollEdit(UpdateView):
 class PollDelete(DeleteView):
     model = Poll
     success_url = '/poll/'
+
+class OptionCreate(CreateView):
+    model = Option
+    fields = ['title']
+    template_name = 'default/poll_form.html'
+    
+    def form_valid(self, form):
+        form.instance.poll_id = self.kwargs['pk']
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return "/poll/{}/".format(self.object.poll_id)
+
+class OptionEdit(UpdateView):
+    model = Option
+    fields = ['title']
+    pk_url_kwarg = 'oid'
+    template_name = 'default/poll_form.html'
+
+    def get_success_url(self):
+        return "/poll/{}/".format(self.object.poll_id)
+
+class OptionDelete(DeleteView):
+    model = Option
+    pk_url_kwarg = 'oid'
+
+    def get_success_url(self):
+        return "/poll/{}/".format(self.object.poll_id)
